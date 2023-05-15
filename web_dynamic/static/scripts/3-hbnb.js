adf7a62523fcbdfbd4fc38e3d519571f4abf6719
Bsakwa <sakwabrian23@gmail.com>
2023-05-15 13:46:59 +0300

    Load place info using ajax

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,61 +1,74 @@
-$(function () {
-  let checked = [];
-  const amenitiesH4 = $('div.amenities h4');
+#!/usr/bin/node
 
-  $('input[type="checkbox"]').on('change', function () {
-    const id = $(this).data('id');
-    const name = $(this).data('name');
+/*
+ * Requests a status from the API
+ */
 
-    if ($(this).is(':checked')) {
-      checked.push({ id: id, name: name });
+const $ = window.$;
+$(document).ready(function () {
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
     } else {
-      checked = checked.filter(item => item.id != id);
+      $('DIV#api_status').removeClass('available');
     }
   });
 
+  /*
+ * Requests a list of all Places from the API
+ * Places must be within the <SECTION> tag
+ * Each Place must be within an <ARTICLE> tag
+ * Each Place description must be in a <DIV> with class="price_by_night"
+ */
+
+  const urlPlaces = 'http://0.0.0.0:5001/api/v1/places_search/';
   $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/status/',
+    url: urlPlaces,
+    type: 'POST',
+    contentType: 'application/json',
     dataType: 'json',
+    data: JSON.stringify({}),
     success: function (data) {
-      if (data.status === 'OK') {
-        $('div#api_status').addClass('available');
-      } else {
-        $('div#api_status').removeClass('available');
+      for (const place of data) {
+        const article = $('<article></article>');
+        const title = $('<div class="title_box"></div>');
+        const name = $('<h2></h2>').text(place.name);
+        const price = $('<div class="price_by_night"></div>').text('$' + place.price_by_night);
+        title.append(name);
+        title.append(price);
+        const info = $('<div class="information"></div>');
+        const maxGuest = $('<div class="max_guest"></div>').text(place.max_guest + ' Guests');
+        const numRooms = $('<div class="number_rooms"></div>').text(place.number_rooms + ' Bedrooms');
+        const numBathrooms = $('<div class="number_bathrooms"></div>').text(place.number_bathrooms + ' Bathrooms');
+        info.append(maxGuest);
+        info.append(numRooms);
+        info.append(numBathrooms);
+        const descript = $('<div class="description"></div>').text(place.description);
+	const description = descript.text();
+	const describe = description.replace(/\r\n/g, "");
+        article.append(title);
+        article.append(info);
+        article.append(describe);
+        $('section.places').append(article);
       }
     }
   });
 
-  $.ajax({
-    method: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    data: JSON.stringify({}),
-    dataType: 'json',
-    headers: {
-    	'Content-Type': 'application/json'
-    },
-    success: function (data) {
-      const html = data.map(place => `
-        <article>
-          <div class="title_box">
-            <h2>${place.name}</h2>
-            <div class="price_by_night">$${place.price_by_night}</div>
-          </div>
-          <div class="information">
-            <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
-            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
-          </div>
-          <div class="description">
-            ${place.description}
-          </div>
-        </article>
-      `).join('');
-      
-      $('section.places').append(html);
-      
-      // Update the amenities h4 element
-      const checkedStr = checked.map(item => item.name).join(', ');
-      amenitiesH4.text(checkedStr);
+  /*
+ * Listens for changes on each input checkbox tag
+ * if checkbox is checked, you must store the Amenity ID in a variable
+ * if checkbox is unchecked, you must remove the Amenity ID from the variable
+ * update the list of Amenities (<div class="amenities">)
+ */
+
+  const amenityDict = {};
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).is(':checked')) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
     }
+    $('.amenities H4').text(Object.values(amenityDict).join(', '));
   });
 });
